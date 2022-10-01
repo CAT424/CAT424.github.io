a23fdffce21855176349793906be24ea3ba6242c
CAT424 <114674326+CAT424@users.noreply.github.com>
2022-10-02 00:05:17 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript")
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.classList.add("small");
+    targetImage.addEventListener("click", function() {
+    alert("点击了图片");
+        targetImage.classList.remove("small");
+});
 });
